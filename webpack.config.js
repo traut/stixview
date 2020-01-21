be15b624d25ef66e473b33c5232b081174f22122
traut <sergey@polzunov.com>
2020-01-21 14:04:22 +0100

    webpack config cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
@@ -13,46 +12,37 @@ const demoFiles = fs.readdirSync(DEMO_SRC_DIR).map(function(filename) {
         template: path.resolve(DEMO_SRC_DIR, filename),
         filename: path.resolve(DEMO_DEST_DIR, filename),
         inject: 'head',
-    }
-})
+    };
+});
 
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'stixview.bundle.js',
-	},
-	module: {
-		rules:[
-			{
+    },
+    module: {
+        rules: [
+            {
                 test: /\.css$/,
                 use: [
-                    'style-loader', 'css-loader'
-                ]
+                    'style-loader',
+                    'css-loader',
+                ],
             },
             {
                 test: /\.svg$/,
-                loader: 'svg-inline-loader'
-            }
-		]
+                loader: 'svg-inline-loader',
+            },
+        ],
     },
     watch: true,
     optimization: {
-        minimize: false
+        minimize: true,
     },
-    plugins: demoFiles.map(function(details) {return new HtmlWebpackPlugin(details)}),
-//    optimization: {
-//        minimizer: [
-//            new TerserPlugin({
-//                cache: true,
-//                parallel: true,
-//                sourceMap: true, // Must be set to true if using source-maps in production
-//                terserOptions: {
-//                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-//                }
-//            }),
-//        ],
-//    }
+    plugins: demoFiles.map(function(details) {
+        return new HtmlWebpackPlugin(details);
+    }),
 };
