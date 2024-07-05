af2b89bd24646b24e0c98ffaeeed527f1dbe5f56
David G <himynamesdave@users.noreply.github.com>
2024-07-05 13:22:27 +0100

    initial commit with some tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Stixview
+## stixview
 
 [![npm version](https://badge.fury.io/js/stixview.svg)](https://badge.fury.io/js/stixview)
 
@@ -16,18 +16,17 @@ If intelligence provider cares about structured machine-readable CTI, the report
 
 The objective of [Stixview](https://github.com/traut/stixview) library is to provide easily embeddable STIX2 graphs with necessary level of interactivity, so that CTI community can create informative and engaging stories.
 
-
 ## Demos
 
 To see Stixview in action, take a look at these demo pages:
-* [STIX2.1 demo](https://traut.github.io/stixview/dist/demos/stix21-demo.html) — sample graph with all STIX 2.1 objects.
-* [Storyline](https://traut.github.io/stixview/dist/demos/story.html) — multiple graphs per page, rendering selected entities from the same STIX bundle.
-* [Viewer](https://traut.github.io/stixview/dist/demos/viewer.html) — graph viewer with custom controls.
-* [Drag-n-drop](https://traut.github.io/stixview/dist/demos/drag-n-drop.html) — graph views with drag-n-drop enabled.
-* [Dark theme graph from inline data](https://traut.github.io/stixview/dist/demos/load-data.html) — rendering graph from inline STIX2 bundle and custom styling.
-* [TLP tags and custom sidebar content renderer](https://traut.github.io/stixview/dist/demos/tags-and-custom-sidebar.html) — TLP marking definitions shown as tags and sidebar is rendered with provided function.
-* [Examples of various configuration settings](https://traut.github.io/stixview/dist/demos/misc.html)
 
+* [STIX2.1 demo](dist/demos/stix21-demo.html) — sample graph with all STIX 2.1 objects.
+* [Storyline](dist/demos/story.html) — multiple graphs per page, rendering selected entities from the same STIX bundle.
+* [Viewer](dist/demos/viewer.html) — graph viewer with custom controls.
+* [Drag-n-drop](dist/demos/drag-n-drop.html) — graph views with drag-n-drop enabled.
+* [Dark theme graph from inline data](dist/demos/load-data.html) — rendering graph from inline STIX2 bundle and custom styling.
+* [TLP tags and custom sidebar content renderer](dist/demos/tags-and-custom-sidebar.html) — TLP marking definitions shown as tags and sidebar is rendered with provided function.
+* [Examples of various configuration settings](dist/demos/misc.html)
 
 ## Usage
 
@@ -43,13 +42,13 @@ or use [unpkg](https://unpkg.com) CDN service:
 <script src="https://unpkg.com/stixview/dist/stixview.bundle.js" type="text/javascript"></script>
 ```
 
-
 ## API
 
 The library relies heavily on [data attributes](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes).
 On page load, Stixview will find all HTML elements with `data-stix-gist-id`, `data-stix-url` or `data-stix-allow-dragdrop` set and use these elements as graph holders.
 
 Example of a graph holder div:
+
 ```html
 <div data-stix-gist-id="6a0fbb0f6e7faf063c748b23f9c7dc62"
      data-show-sidebar=true
@@ -65,86 +64,56 @@ Every holder element _must have_ one of `data-stix-gist-id`, `data-stix-url` or 
 
 Stixview supports these `data-` attributes:
 
-`stix-gist-id` — id of a gist that contains STIX2 bundle. if `gist-file` is not specified, first file will be used.
-
-`gist-file` — name of a file from gist to be used as STIX2 bundle. Only used if `stix-gist-id` is set.
-
-`stix-url` — URL pointing to a remote STIX2 bundle JSON file.
-
-`stix-allow-dragdrop` (`false` by default) — enable ability to drag-n-drop STIX2 bundle into the graph element. If this property is set to `true` and `stix-gist-id` and `stix-url` are not specified, empty graph will be rendered.
-
-`caption` — a title of the graph. Header is not shown if `caption` is not set.
-
-`show-footer` (`true` by default) — show a footer with the download links to STIX2 bundle and PNG file.
-
-`show-sidebar` (`true` by default) — enable a sidebar with object details, opened when object is clicked on.
-
-`show-tlp-as-tags` (`true` by default) — show connected TLP marking definition objects as tags on entities.
-
-`show-marking-nodes` (`true` by default) — show the marking definition nodes.
-
-`show-labels` (`true` by default) — show node labels.
-
-`show-idrefs` (`false` by default) — show placeholder objects for ids mentioned in the relationships but not present in a bundle.
-
-`graph-layout` (`cola` by default) — name of the graph layout algorythm. Supported algorythms are `cola` , `klay`, `cose-bilkent`, `cise`, `grid` and `dagre`.
-
-`enable-mouse-zoom` (`true` by default) — enable mouse wheel zoom.
-
-`enable-panning` (`true` by default) — enable panning in the graph. If `false`, a graph has a fixed view. Users will still be able to drag nodes around.
-
-`highlighted-objects` — a string with comma-separated STIX2 ids. If set, graph will contain _only objects with ids listed_.
-
-`hidden-objects` — a string with comma-separated STIX2 ids. If set, objects with ids listed will be skipped and not rendered in the graph.
-
-`min-zoom` (`0.3` by default) — minimum allowed zoom.
-
-`max-zoom` (`2.5` by default) — maximum allowed zoom.
-
-`graph-width` (all available width by default) — width of a graph element. Both pixel and % values are supported (see misc demo page for an example).
-
-`graph-height` (`600` pixels by default) — height of a graph element. Both pixel and % values are supported (see misc demo page for an example).
-
+* `stix-gist-id` — id of a gist that contains STIX2 bundle. if `gist-file` is not specified, first file will be used.
+* `gist-file` — name of a file from gist to be used as STIX2 bundle. Only used if `stix-gist-id` is set.
+* `stix-url` — URL pointing to a remote STIX2 bundle JSON file.
+* `stix-allow-dragdrop` (`false` by default) — enable ability to drag-n-drop STIX2 bundle into the graph element. If this property is set to `true` and `stix-gist-id` and `stix-url` are not specified, empty graph will be rendered.
+* `caption` — a title of the graph. Header is not shown if `caption` is not set.
+* `show-footer` (`true` by default) — show a footer with the download links to STIX2 bundle and PNG file.
+* `show-sidebar` (`true` by default) — enable a sidebar with object details, opened when object is clicked on.
+* `show-tlp-as-tags` (`true` by default) — show connected TLP marking definition objects as tags on entities.
+* `show-marking-nodes` (`true` by default) — show the marking definition nodes.
+* `show-labels` (`true` by default) — show node labels.
+* `show-idrefs` (`false` by default) — show placeholder objects for ids mentioned in the relationships but not present in a bundle.
+* `graph-layout` (`cola` by default) — name of the graph layout algorythm. Supported algorythms are `cola` , `klay`, `cose-bilkent`, `cise`, `grid` and `dagre`.
+* `enable-mouse-zoom` (`true` by default) — enable mouse wheel zoom.
+* `enable-panning` (`true` by default) — enable panning in the graph. If `false`, a graph has a fixed view. Users will still be able to drag nodes around.
+* `highlighted-objects` — a string with comma-separated STIX2 ids. If set, graph will contain _only objects with ids listed_.
+* `hidden-objects` — a string with comma-separated STIX2 ids. If set, objects with ids listed will be skipped and not rendered in the graph.
+* `min-zoom` (`0.3` by default) — minimum allowed zoom.
+* `max-zoom` (`2.5` by default) — maximum allowed zoom.
+* `graph-width` (all available width by default) — width of a graph element. Both pixel and % values are supported (see misc demo page for an example).
+* `graph-height` (`600` pixels by default) — height of a graph element. Both pixel and % values are supported (see misc demo page for an example).
 
 ### Browser object
 
 The library, when used in a browser, will register `stixview` variable on `window` object with these properties:
 
-`registry` — a registry of graphs initiated on a page.
-
-`onInit(selector, callback)` – listener hook for graph's init event on a DOM element that matches provided `selector` value (see [demo](https://traut.github.io/stixview/dist/demos/viewer.html) for usage example). Callback receive instance of a graph interface.
-
-`onLoad(selector, callback)` – listener hook for graph's load event on a DOM element that matches provided `selector` value (see [demo](https://traut.github.io/stixview/dist/demos/viewer.html) for usage example). Callback receive instance of a graph interface.
-
-`init(element, properties, initCallback, loadCallback)` — method that initiates a graph view in specified `element` with provided `properties` that override defaults (see [demo](https://traut.github.io/stixview/dist/demos/load-data.html) for usage example).
-
+* `registry` — a registry of graphs initiated on a page.
+* `onInit(selector, callback)` – listener hook for graph's init event on a DOM element that matches provided `selector` value (see [demo](https://traut.github.io/stixview/dist/demos/viewer.html) for usage example). Callback receive instance of a graph interface.
+* `onLoad(selector, callback)` – listener hook for graph's load event on a DOM element that matches provided `selector` value (see [demo](https://traut.github.io/stixview/dist/demos/viewer.html) for usage example). Callback receive instance of a graph interface.
+* `init(element, properties, initCallback, loadCallback)` — method that initiates a graph view in specified `element` with provided `properties` that override defaults (see [demo](https://traut.github.io/stixview/dist/demos/load-data.html) for usage example).
 
 ### Graph object
 
 Graph is an object with properties:
 
-`cy` – [cytoscape.js](http://js.cytoscape.org) graph object.
+* `cy` – [cytoscape.js](http://js.cytoscape.org) graph object.
+* `element` — DOM element that holds a graph.
+* `dataProps` – configured data filtering properties.
+* `viewProps` – configured viewing properties.
+* `runLayout(name)` — run specific layout on a graph.
+* `reloadData()` — reload graph data with new data filtering properties.
+* `fit()` — fit graph fully into a graph view.
+* `toggleLabels(<bool-value>)` — show / hide node labels.
+* `toggleLoading(<bool-value>)` — show / hide loading overlay.
+* `loadData(data)` — load STIX2 bundle from `data` JSON object and render on a graph.
+* `loadDataFromFile(file)` — load STIX2 bundle from `file` file object and render on a graph.
+* `loadDataFromUrl(url)` — load STIX2 bundle from remote URL and render on a graph.
+* `loadDataFromParamUrl(paramName)` — load STIX2 bundle from remote URL, configured in HTTP GET paramter with name in `paramName` and render on a graph.
 
-`element` — DOM element that holds a graph.
+## Build
 
-`dataProps` – configured data filtering properties.
-
-`viewProps` – configured viewing properties.
-
-`runLayout(name)` — run specific layout on a graph.
-
-`reloadData()` — reload graph data with new data filtering properties.
-
-`fit()` — fit graph fully into a graph view.
-
-`toggleLabels(<bool-value>)` — show / hide node labels.
-
-`toggleLoading(<bool-value>)` — show / hide loading overlay.
-
-`loadData(data)` — load STIX2 bundle from `data` JSON object and render on a graph.
-
-`loadDataFromFile(file)` — load STIX2 bundle from `file` file object and render on a graph.
-
-`loadDataFromUrl(url)` — load STIX2 bundle from remote URL and render on a graph.
-
-`loadDataFromParamUrl(paramName)` — load STIX2 bundle from remote URL, configured in HTTP GET paramter with name in `paramName` and render on a graph.
+```shell
+yarn build
+```
--- src/icons.js
+++ src/icons.js
@@ -1,5 +1,6 @@
 import _ from 'underscore';
 
+// SDOs
 const attackPatternIcon = require(
     '!svg-inline-loader?removeSVGTagAttrs=false!../icons/attack-pattern.svg');
 const campaignIcon = require(
@@ -30,34 +31,86 @@ const observedDataIcon = require(
     '!svg-inline-loader?removeSVGTagAttrs=false!../icons/observed-data.svg');
 const opinionIcon = require(
     '!svg-inline-loader?removeSVGTagAttrs=false!../icons/opinion.svg');
-const relationshipIcon = require(
-    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/relationship.svg');
 const reportIcon = require(
     '!svg-inline-loader?removeSVGTagAttrs=false!../icons/report.svg');
-const sightingIcon = require(
-    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/sighting.svg');
 const threatActorIcon = require(
     '!svg-inline-loader?removeSVGTagAttrs=false!../icons/threat-actor.svg');
 const toolIcon = require(
     '!svg-inline-loader?removeSVGTagAttrs=false!../icons/tool.svg');
 const vulnerabilityIcon = require(
     '!svg-inline-loader?removeSVGTagAttrs=false!../icons/vulnerability.svg');
-
-const hypothesisIcon = require(
-    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/x-eclecticiq-hypothesis.svg');
+// DOGESEC custom SDO
+const weaknessIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/weakness.svg');
+// SROs
+const relationshipIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/relationship.svg');
+const sightingIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/sighting.svg');
+// SCOs
+const artifactIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/artifact.svg');
+const autonomousSystemIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/autonomous-system.svg');
+const directoryIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/directory.svg');
+const domainNameIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/domain-name.svg');
+const emailAddrIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/email-addr.svg');
+const emailMessageIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/email-message.svg');
+const fileIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/file.svg');
+const ipv4AddrIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/ipv4-addr.svg');
+const ipv6AddrIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/ipv6-addr.svg');
+const macAddrIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/mac-addr.svg');
+const mutexIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/mutex.svg');
+const networkTrafficIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/network-traffic.svg');
+const processIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/process.svg');
+const softwareIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/software.svg');
+const urlIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/url.svg');
+const userAccountIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/user-account.svg');
+const windowsRegistryKeyIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/windows-registry-key.svg');
+const x509CertificateIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/x509-certificate.svg');
+// DOGESEC custom SCOs
+const bankAccountIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/bank-account.svg');
+const bankCardIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/bank-card.svg');
+const cryptocurrencyTransactionIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/cryptocurrency-transaction.svg');
+const cryptocurrencyWalletIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/cryptocurrency-wallet.svg');
+const phoneNumberIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/phone-number.svg');
+const userAgentIcon = require(
+    '!svg-inline-loader?removeSVGTagAttrs=false!../icons/user-agent.svg');
 
 function encodeSvg(icon) {
     return 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(icon);
 }
 
 const iconPerType = {
+    // SDOs
     'attack-pattern': {
         color: '#2277b5',
         shape: 'ellipse',
         image: encodeSvg(attackPatternIcon),
     },
     'campaign': {
-        color: '#50b682',
+        color: '#50b61e',
         shape: 'ellipse',
         image: encodeSvg(campaignIcon),
     },
@@ -72,32 +125,32 @@ const iconPerType = {
         image: encodeSvg(groupingIcon),
     },
     'identity': {
-        color: '#9c9d9d',
+        color: '#9c9afe',
         shape: 'ellipse',
         image: encodeSvg(identityIcon),
     },
     'incident': {
-        color: '#fcb617',
+        color: '#fbb616',
         shape: 'ellipse',
         image: encodeSvg(incidentIcon),
     },
     'indicator': {
-        color: '#e38850',
+        color: '#dc9547',
         shape: 'ellipse',
         image: encodeSvg(indicatorIcon),
     },
     'infrastructure': {
-        color: '#aed7c0',
+        color: '#aed7bf',
         shape: 'ellipse',
         image: encodeSvg(infrastructureIcon),
     },
     'intrusion-set': {
-        color: '#39b2c1',
+        color: '#38b2c1',
         shape: 'ellipse',
         image: encodeSvg(intrusionSetIcon),
     },
     'location': {
-        color: '#505657',
+        color: '#fc9f9d',
         shape: 'ellipse',
         image: encodeSvg(locationIcon),
     },
@@ -107,39 +160,29 @@ const iconPerType = {
         image: encodeSvg(malwareAnalysisIcon),
     },
     'malware': {
-        color: '#d3a3cb',
+        color: '#d4a3cb',
         shape: 'ellipse',
         image: encodeSvg(malwareIcon),
     },
     'note': {
-        color: '#505657',
+        color: '#88c881',
         shape: 'ellipse',
         image: encodeSvg(noteIcon),
     },
     'observed-data': {
-        color: '#000000',
+        color: '#fcccb8',
         shape: 'ellipse',
         image: encodeSvg(observedDataIcon),
     },
     'opinion': {
-        color: '#505657',
+        color: '#909dc7',
         shape: 'ellipse',
         image: encodeSvg(opinionIcon),
     },
-    'relationship': {
-        color: '#cdd6d8',
-        shape: 'ellipse',
-        image: encodeSvg(relationshipIcon),
-    },
     'report': {
-        color: '#769279',
+        color: '#779279',
         shape: 'ellipse',
         image: encodeSvg(reportIcon),
-    },
-    'sighting': {
-        color: '#eb5e2a',
-        shape: 'ellipse',
-        image: encodeSvg(sightingIcon),
     },
     'threat-actor': {
         color: '#e61b5c',
@@ -156,15 +199,157 @@ const iconPerType = {
         shape: 'ellipse',
         image: encodeSvg(vulnerabilityIcon),
     },
+    // DOGESEC custom SDO
+    'weakness': {
+        color: '#5e3180',
+        shape: 'ellipse',
+        image: encodeSvg(weaknessIcon),
+    },
+    // SROs
+    'relationship': {
+        color: '#94f38b',
+        shape: 'ellipse',
+        image: encodeSvg(relationshipIcon),
+    },
+    'sighting': {
+        color: '#eb5e2a',
+        shape: 'ellipse',
+        image: encodeSvg(sightingIcon),
+    },
+    // SCOs
+    'artifact': {
+        color: '#95e5fa',
+        shape: 'ellipse',
+        image: encodeSvg(artifactIcon),
+    },
+    'autonomous-system': {
+        color: '#a1f880',
+        shape: 'ellipse',
+        image: encodeSvg(autonomousSystemIcon),
+    },
+    'directory': {
+        color: '#b7f5ce',
+        shape: 'ellipse',
+        image: encodeSvg(directoryIcon),
+    },
+    'domain-name': {
+        color: '#ffb9a7',
+        shape: 'ellipse',
+        image: encodeSvg(domainNameIcon),
+    },
+    'email-addr': {
+        color: '#9180f2',
+        shape: 'ellipse',
+        image: encodeSvg(emailAddrIcon),
+    },
+    'email-message': {
+        color: '#f981e5',
+        shape: 'ellipse',
+        image: encodeSvg(emailMessageIcon),
+    },
+    'file': {
+        color: '#c794bb',
+        shape: 'ellipse',
+        image: encodeSvg(fileIcon),
+    },
+    'ipv4-addr': {
+        color: '#de82ab',
+        shape: 'ellipse',
+        image: encodeSvg(ipv4AddrIcon),
+    },
+    'ipv6-addr': {
+        color: '#de82ab',
+        shape: 'ellipse',
+        image: encodeSvg(ipv6AddrIcon),
+    },
+    'mac-addr': {
+        color: '#f7b8cb',
+        shape: 'ellipse',
+        image: encodeSvg(macAddrIcon),
+    },
+    'mutex': {
+        color: '#f0e499',
+        shape: 'ellipse',
+        image: encodeSvg(mutexIcon),
+    },
+    'network-traffic': {
+        color: '#84cff0',
+        shape: 'ellipse',
+        image: encodeSvg(networkTrafficIcon),
+    },
+    'process': {
+        color: '#bbc799',
+        shape: 'ellipse',
+        image: encodeSvg(processIcon),
+    },
+    'software': {
+        color: '#e991ca',
+        shape: 'ellipse',
+        image: encodeSvg(softwareIcon),
+    },
+    'url': {
+        color: '#cecff1',
+        shape: 'ellipse',
+        image: encodeSvg(urlIcon),
+    },
+    'user-account': {
+        color: '#d5bf84',
+        shape: 'ellipse',
+        image: encodeSvg(userAccountIcon),
+    },
+    'windows-registry-key': {
+        color: '#84c4aa',
+        shape: 'ellipse',
+        image: encodeSvg(windowsRegistryKeyIcon),
+    },
+    'x509-certificate': {
+        color: '#f6a0f2',
+        shape: 'ellipse',
+        image: encodeSvg(x509CertificateIcon),
+    },
+    // DOGESEC SCOs
+    'bank-account': {
+        color: '#e8e4aa',
+        shape: 'ellipse',
+        image: encodeSvg(bankAccountIcon),
+    },
+    'bank-card': {
+        color: '#91b2b5',
+        shape: 'ellipse',
+        image: encodeSvg(bankCardIcon),
+    },
+    'cryptocurrency-transaction': {
+        color: '#dee9a7',
+        shape: 'ellipse',
+        image: encodeSvg(cryptocurrencyTransactionIcon),
+    },
+    'cryptocurrency-wallet': {
+        color: '#9cdab8',
+        shape: 'ellipse',
+        image: encodeSvg(cryptocurrencyWalletIcon),
+    },
+    'phone-number': {
+        color: '#e2bdef',
+        shape: 'ellipse',
+        image: encodeSvg(phoneNumberIcon),
+    },
+    'user-agent': {
+        color: '#98c7ef',
+        shape: 'ellipse',
+        image: encodeSvg(userAgentIcon),
+    },
+    // SMOs
+    'extension-definition': {
+        color: '#b0d799',
+        shape: 'tag',
+    },
     'marking-definition': {
         color: '#72d1fb',
         shape: 'tag',
     },
-    // custom EclecticIQ object
-    'x-eclecticiq-hypothesis': {
-        color: '#009688',
-        shape: 'ellipse',
-        image: encodeSvg(hypothesisIcon),
+    'language-content': {
+        color: '#ff1744',
+        shape: 'tag',
     },
     // idref placeholder node
     'idref': {
@@ -172,33 +357,6 @@ const iconPerType = {
         shape: 'octagon',
     },
 };
-
-
-// Cyber-observable Objects colors
-
-const cyberObsColors = {
-    'artifact': '#155126',
-    'autonomous-system': '#99e5ad',
-    'directory': '#2d5192',
-    'domain-name': '#4cf185',
-    'email-addr': '#489f90',
-    'email-message': '#abd533',
-    'mime-part-type': '#4233a6',
-    'file': '#bfd6fa',
-    'archive-ext': '#7487fb',
-    'ipv4-addr': '#c338c5',
-    'ipv6-addr': '#eca2d5',
-    'mac-addr': '#782857',
-    'mutex': '#2c97e0',
-    'network-traffic': '#39970e',
-    'process': '#d6061a',
-    'software': '#e69976',
-    'url': '#914c0f',
-    'user-account': '#f4d403',
-    'windows-registry-key': '#f23387',
-    'x509-certificate': '#e0d59b',
-};
-
 
 const unknownIconTmpl = `
 <svg width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -219,7 +377,6 @@ const unknownIconTmpl = `
 function getPlaceholderIcon(nodeType) {
     const template = _.template(unknownIconTmpl);
     const letter = nodeType.charAt(0).toUpperCase();
-    const color = cyberObsColors[nodeType] || '#B99435';
     return {
         color: color,
         shape: 'ellipse',
